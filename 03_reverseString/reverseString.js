3d9b3f7c81f09ba75bfb528dee4b689fd353f81c
Adam Holzer <shortyforcs@gmail.com>
2022-10-08 14:03:32 -0700

    finish 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let answer = '';
+    for(let i = word.length - 1; i >= 0; i--){
+        answer += word[i]
+    }
+    return answer;
 };
 
 // Do not edit below this line
