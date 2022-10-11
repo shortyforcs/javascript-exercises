77a1444f3e8413fa846bb70b3f2acc0eebb78fd4
Adam Holzer <shortyforcs@gmail.com>
2022-10-11 12:42:10 -0700

    remove extra solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...toRemove) {
+    const answer = []
+    for(let i = 0; i < array.length; i++){
+        //if toRemove includes the current letter of array, skip it and keep going
+        if(toRemove.includes(array[i])){
+            continue;
+        }
+        //otherwise add it to answers
+        answer.push(array[i]);
+    }
+    return answer;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
