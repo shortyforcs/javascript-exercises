c00f99ac86ef5d5af63205450b87aecf78929bdb
Adam Holzer <shortyforcs@gmail.com>
2022-10-12 14:29:50 -0700

    exercise 7 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(tempInF) {
+  let tempInC = 0
+  tempInC = (tempInF - 32) * (5/9)
+  // console.log(tempInC)
+  return Math.round(tempInC * 10) / 10
 };
 
-const ctof = function() {
-
+const ctof = function(tempInC) {
+  let tempInF = 0
+  tempInF = tempInC * (9/5) + 32
+  // console.log(tempInF)
+  return Math.round(tempInF * 10) / 10
 };
 
 // Do not edit below this line
