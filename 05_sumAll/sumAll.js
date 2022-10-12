12c472eeb54e9762446b79ad2fbe2c94e4f76646
Adam Holzer <shortyforcs@gmail.com>
2022-10-12 13:15:28 -0700

    exercise 5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    //precondition checks
+    //if any are not numbers
+    if(typeof num1 != 'number' || typeof num2 != 'number')
+        return "ERROR";
+    //if any numbers are negative
+    if(num1 < 0 || num2 < 0)
+        return "ERROR";
+    let sum = 0;
+    //figure out which one is higher and lower
+    const lower = (num1 <= num2) ? num1 : num2;
+    const higher = (num1 > num2) ? num1 : num2;
+    console.log(`lower: ${lower}`)
+    console.log(`higher: ${higher}`)
+    for(let i = lower; i <= higher; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
